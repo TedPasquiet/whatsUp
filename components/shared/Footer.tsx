a80a62782fcb6401d226afd5514a85c2162c5608
TedPasquiet <tpasquiet@hotmail.fr>
2024-04-21 17:02:15 +0200

    Footer
    Creation footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+import image from "../../public/assets/images/logo.svg"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-beetween flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
+          src={image}
+          alt="logo"
+          width={128}
+          height={38}
+          />
+        </Link>
+        <p>2024 WhatsUp</p>
+      </div>
+    </footer>
   )
 }
 
